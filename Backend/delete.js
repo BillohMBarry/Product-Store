fbb8b07df1d07c6ed64143194201d526d0032773
BillohMBarry <196953513+BillohMBarry@users.noreply.github.com.>
2025-04-18 15:25:28 +0000

    create error handling for delete product

--- Backend/delete.js
+++ Backend/delete.js
@@ -1,11 +1,25 @@
 import express from "express"
-
+import { Product } from "./models/productModel.js"
 const app = express()
 
 const deleteRequest = app.delete("/api/products/:id", async (req, res) => {
     // res.send("Delete Request")
     const { id } = req.params
     console.log(`id is ${id}`)
+
+    try {
+        const product = await Product.findByIdAndDelete(id)
+        res.status(200).json({
+            success: true,
+            message: "Product Deleted Successfully",
+            data : product
+        })
+    } catch(error) {
+        res.status(500).json({
+            message: "Error in deleting product",
+            data : error
+        })
+    }
 })
 
 export default deleteRequest
